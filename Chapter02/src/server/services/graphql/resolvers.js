051c6deabbef0eb2a7afcfeb54809fa77ab56468
guochao <463561248@qq.com>
2022-04-11 00:16:25 +0800

    调整代码

--- Chapter02/src/server/services/graphql/resolvers.js
+++ Chapter02/src/server/services/graphql/resolvers.js
@@ -1,4 +1,4 @@
-import logger from '../../helpers/logger';
+// import logger from '../../helpers/logger';
 
 let posts = [
     {
@@ -22,21 +22,22 @@ let posts = [
 const resolvers = {
     RootQuery: {
         posts(root, args, context) {
-            return posts;
+            // return posts;
+            return []
         },
     },
-    RootMutation: {
-        addPost(root, { post, user }, context) {
-            const postObject = {
-                ...post,
-                user,
-                id: posts.length + 1,
-            };
-            posts.push(postObject);
-            logger.log({ level: 'info', message: 'Post was created' });
-            return postObject;
-        },
-    },
+    // RootMutation: {
+    //     addPost(root, { post, user }, context) {
+    //         const postObject = {
+    //             ...post,
+    //             user,
+    //             id: posts.length + 1,
+    //         };
+    //         posts.push(postObject);
+    //         logger.log({ level: 'info', message: 'Post was created' });
+    //         return postObject;
+    //     },
+    // },
 };
 
 export default resolvers;
